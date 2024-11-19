1ba8a0b8b5391a39041ab1b629bc0334f2557867
Siphiwo10 <siphiwoj10@gmail.com>
2024-11-19 09:51:11 +0000

    adding whitspace

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,3 +1,7 @@
+#!/usr/bin/python3
+"""matrix"""
+
+
 def rotate_2d_matrix(matrix):
     """
     Rotates an n x n 2D matrix 90 degrees clockwise in-place.
@@ -5,12 +9,12 @@ def rotate_2d_matrix(matrix):
     :param matrix: 2D list to rotate
     """
     n = len(matrix)
-    
+
     # Transpose the matrix (swap rows and columns)
     for i in range(n):
         for j in range(i, n):
             matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
-    
+
     # Reverse each row to achieve a 90-degree clockwise rotation
     for row in matrix:
         row.reverse()
