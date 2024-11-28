72bb0a4b0d7e4c3700279cf18dc76a45cd94e1e2
Siphiwo10 <siphiwoj10@gmail.com>
2024-11-28 18:02:25 +0000

    adding and fixing storage output

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -3,17 +3,18 @@
 
 
 def makeChange(coins, total):
-    """main def file"""
+    """main def file for changes"""
     if total <= 0:
         return 0
-
-    min_coins = [float('inf')] * (total + 1)
-    min_coins[0] = 0
-
-    for coin in coins:
-        for amount in range(coin, total + 1):
-            min_coins[amount] = min(
-                    min_coins[amount],
-                    min_coins[amount - coin] + 1)
-
-    return min_coins[total] if min_coins[total] != float('inf') else -1
+    check = 0
+    ret = 0
+    coins.sort(reverse=True)
+    for i in coins:
+        while check < total:
+            check += i
+            ret += 1
+        if check == total:
+            return ret
+        check -= i
+        ret -= 1
+    return -1
